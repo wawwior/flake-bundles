8dee6dbc35d35f2da492177affba1f7f7aa91930
wawwior <45405580+wawwior@users.noreply.github.com>
2026-09-07 21:59:19 +0200

    feat: mbr support

--- lib/resolvers/disko.nix
+++ lib/resolvers/disko.nix
@@ -4,6 +4,7 @@
     {
       disks ? [ ],
       swap ? null,
+      efi ? true,
       ...
     }:
     let
@@ -44,6 +45,11 @@
                   content = {
                     type = "gpt";
                     partitions = {
+                      boot = lib.mkIf (!efi) {
+                        name = "BOOT";
+                        size = "1M";
+                        type = "EF02";
+                      };
                       esp = {
                         name = "ESP";
                         size = "500M";
