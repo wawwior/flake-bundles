ac48ebbe64d6999b125f833460cee528e279bfcc
wawwior <45405580+wawwior@users.noreply.github.com>
2026-09-08 13:21:47 +0200

    WIP feat: clear task separation

--- lib/resolvers/home.nix
+++ lib/resolvers/home.nix
@@ -1,29 +1,17 @@
-{ inputs, lib, ... }:
+{ inputs, ... }:
 {
   name = "home";
   host =
     {
       aspects ? [ ],
-      users ? { },
-      version ? null,
       ...
     }:
-    let
-      users' = builtins.mapAttrs (_: value: value.aspects) users;
-    in
     [
       {
         imports = [ inputs.home-manager.nixosModules.default ];
 
         home-manager = {
-          sharedModules = map (aspect: aspect.resolve { class = "home"; }) aspects ++ [
-            {
-              home.stateVersion = lib.mkIf (version != null) version;
-            }
-          ];
-          users = builtins.mapAttrs (_: aspects: {
-            imports = map (aspect: aspect.resolve { class = "home"; }) aspects;
-          }) users';
+          sharedModules = map (aspect: aspect.resolve { class = "home"; }) aspects;
         };
       }
     ];
--- lib/resolvers/nixos.nix
+++ lib/resolvers/nixos.nix
@@ -1,26 +1,11 @@
-{ lib, ... }: {
+{ ... }: {
   name = "nixos";
   host =
     {
-      users ? { },
       aspects ? [ ],
-      version ? null,
       ...
     }:
     let
-      users' = builtins.mapAttrs (_: value: {
-        inherit (value) normal name;
-      }) users;
-      aspects' = aspects ++ (lib.flatten (lib.mapAttrsToList (_: value: value.aspects) users));
     in
-    (map (aspect: aspect.resolve { class = "nixos"; }) aspects')
-    ++ [
-      {
-        system.stateVersion = lib.mkIf (version != null) version;
-        users.users = builtins.mapAttrs (name: value: {
-          name = value.name or name;
-          isNormalUser = value.normal;
-        }) users';
-      }
-    ];
+    (map (aspect: aspect.resolve { class = "nixos"; }) aspects);
 }
