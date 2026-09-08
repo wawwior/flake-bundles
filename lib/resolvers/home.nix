7e33807bb090efbed3919a4d8539f5a227389bc9
wawwior <45405580+wawwior@users.noreply.github.com>
2026-09-08 13:56:46 +0200

    Revert "WIP feat: clear task separation"
    This reverts commit ac48ebbe64d6999b125f833460cee528e279bfcc.

--- lib/resolvers/home.nix
+++ lib/resolvers/home.nix
@@ -1,17 +1,29 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 {
   name = "home";
   host =
     {
       aspects ? [ ],
+      users ? { },
+      version ? null,
       ...
     }:
+    let
+      users' = builtins.mapAttrs (_: value: value.aspects) users;
+    in
     [
       {
         imports = [ inputs.home-manager.nixosModules.default ];
 
         home-manager = {
-          sharedModules = map (aspect: aspect.resolve { class = "home"; }) aspects;
+          sharedModules = map (aspect: aspect.resolve { class = "home"; }) aspects ++ [
+            {
+              home.stateVersion = lib.mkIf (version != null) version;
+            }
+          ];
+          users = builtins.mapAttrs (_: aspects: {
+            imports = map (aspect: aspect.resolve { class = "home"; }) aspects;
+          }) users';
         };
       }
     ];
